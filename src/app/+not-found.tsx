81d99fbb37f68934a0741b7eae6732bbe2da71c3
MrCodeBS <singhbhupider206@gmail.com>
2026-05-26 14:07:34 +0200

    fix: update NotFoundScreen component structure and styles

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -5,7 +5,26 @@ export default function NotFoundScreen() {
     return (
         <>
         <Stack.Screen options={{ title: 'Oops! Not Found'}}/>
-        <View S
+        <View style={styles.container}>
+            <Link href="/" style={styles.button} >
+            Go Back to Home Screen!
+            </Link>
+        </View>
         </>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
