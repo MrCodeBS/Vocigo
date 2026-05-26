c874f26ad4f9cd7b55d71cff3e59a60aa4ba688f
MrCodeBS <singhbhupider206@gmail.com>
2026-05-26 13:58:59 +0200

    chore: update dependencies and refactor app structure
    - Updated package.json to use newer versions of Expo and React Native dependencies.
    - Added a new NotFoundScreen component for handling 404 errors.
    - Refactored the layout to use expo-router for navigation.
    - Created an AboutScreen component for the About page.
    - Simplified the Index component to include a link to the About page.
    - Added a new Voci model interface for better type definitions.

---
+++ src/app/+not-found.tsx
@@ -0,0 +1,11 @@
+import {View, StyleSheet } from 'react-native';
+import {Link, Stack} from 'expo-router';
+
+export default function NotFoundScreen() {
+    return (
+        <>
+        <Stack.Screen options={{ title: 'Oops! Not Found'}}/>
+        <View S
+        </>
+    )
+}
