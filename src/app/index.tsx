612816b0eeaec35e95618860bcf0fd25327ec90e
MrCodeBS <singhbhupider206@gmail.com>
2026-05-26 15:19:12 +0200

    refactor: reorder imports and improve code formatting in Index component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from 'expo-router';
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text} >Home Screen</Text>
+      <Text style={styles.text}>Home Screen</Text>
       <Link href="/about" style={styles.button}>
-      Go to About Screen
+        Go to About Screen
       </Link>
     </View>
   );
@@ -14,16 +14,27 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
     color: "#fff",
   },
+  title: {
+    color: "#fff",
+    fontSize: 48,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  subtitle: {
+    color: "#a0a0a0",
+    fontSize: 20,
+    marginBottom: 30,
+  },
   button: {
     fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
-  }
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
 });
