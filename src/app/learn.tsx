ba99b0683ea9fe31cd98dd81c0e13d9e7bd67bca
MrCodeBS <singhbhupider206@gmail.com>
2026-05-27 09:54:28 +0200

    refactor: improve code formatting and consistency in LearnScreen component

--- src/app/learn.tsx
+++ src/app/learn.tsx
@@ -1,24 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
-export default function LearnScreen(){
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Vokabeln lernen</Text>
-            </View>
-    );
+export default function LearnScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Vokabeln lernen</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#25292e',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        color: "#fff",
-        fontWeight: "bold",
-        fontSize: 42,
-        marginBottom: 5,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 60,
+    paddingHorizontal: 20,
+  },
+  title: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 42,
+    marginBottom: 5,
+  },
 });
